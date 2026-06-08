3391620d966af69acc0fb72845666ae099c0b95e
Jung <viggoj@naver.com>
2026-06-08 11:09:24 +0900

    안전계획 -> 차량계획 setting modify

--- views/settings_view.py
+++ views/settings_view.py
@@ -363,7 +363,7 @@ def SettingsView(page: ft.Page):
                                         color=ft.Colors.BLUE_GREY_500),
                         title=ft.Text("앱 정보",
                                       weight=ft.FontWeight.BOLD, size=15),
-                        subtitle=ft.Text("버전: 1.0.2\n개발: DTRO 안전계획팀",
+                        subtitle=ft.Text("버전: 1.0.0\n개발: DTRO 차량계획팀",
                                          size=13),
                     ),
                     ft.Divider(height=20, color=ft.Colors.BLACK12),
